d1baf0126779f4623ea9d4e6416c6a32be4e6fd6
Andrés Maldonado <andresio_mo@hotmail.com>
2017-07-19 20:35:23 +0200

    Update main_url.py

--- main_url.py
+++ main_url.py
@@ -7,9 +7,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('irc.apps.website.urls')),
     # url(r'^', include('wild_unit.apps.user_profile.urls')),
-    url(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    url(r'^7205846B147B4F20E2FAF76A31D8C864\.txt/$', TemplateView.as_view(template_name='7205846B147B4F20E2FAF76A31D8C864.txt', content_type='text/plain')),
+    #url(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    #url(r'^7205846B147B4F20E2FAF76A31D8C864\.txt/$', TemplateView.as_view(template_name='7205846B147B4F20E2FAF76A31D8C864.txt', content_type='text/plain')),
     # SLUG
     # url(r'^', include('wild_unit.apps.blog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
