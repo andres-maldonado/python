471e95a8230a51dba81ab790971a675876c66e5e
Andrés Maldonado <andresio_mo@hotmail.com>
2017-07-19 20:37:43 +0200

    Update main_url.py

--- main_url.py
+++ main_url.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
