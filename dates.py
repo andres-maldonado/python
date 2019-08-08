1f0f3962479e9a746e706269b5bbf9c386ec5dc6
Andrés Maldonado <contact.a.maldonado@gmail.com>
2019-08-08 09:14:10 -0400

    Create dates.py

---
+++ dates.py
@@ -0,0 +1,5 @@
+import pytz
+import calendar
+from datetime import date, timedelta, datetime
+# ADDING UTC TIME ZONE TO NAIVE DATETIME (RETURNS LIKE AWARE BUT +00)  >> DATETIME.astimezone(pytz.timezone(TIME_ZONE))
+deadline = DATETIME.astimezone(pytz.timezone(TIME_ZONE))
