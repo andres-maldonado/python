cdb84597eec9580b984c16da1aff42b98c9bac0b
Andrés Maldonado <contact.a.maldonado@gmail.com>
2019-08-22 14:41:39 -0400

    Update dates.py

--- dates.py
+++ dates.py
@@ -3,3 +3,21 @@ import calendar
 from datetime import date, timedelta, datetime
 # ADDING UTC TIME ZONE TO NAIVE DATETIME (RETURNS LIKE AWARE BUT +00)  >> DATETIME.astimezone(pytz.timezone(TIME_ZONE))
 deadline = DATETIME.astimezone(pytz.timezone(TIME_ZONE))
+
+
+# Localtime to UTC Time
+from django.utils import timezone
+import pytz
+
+timezone.now() # equivale a un objeto datetime en UTC
+
+my_time = datetime.now() # Crea un datetime limpio, sin utc
+time_zone = pytz.timezone(TIME_ZONE) # jala timezone desde donde se recibe my_time
+localized = time_zone.localize(my_time) # localiza my_time, ahora es un objecto consciente de su UTC
+my_time_utc = localized.astimezone(pytz.utc) # se convierte en UTC, my_time_utc == timezone.now(), listo para ser guardado
+
+##############
+# COMPRIMIDO #
+##############
+my_time = datetime.now() 
+my_time_utc = pytz.timezone(TIME_ZONE).localize(my_time).astimezone(pytz.utc)
